6ad82687fe26cd7868d20e72530af756a569df99
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2026-02-18 19:51:35 -0300

    🔍 Update configuration and layout: Add sitemap integration, enhance SEO metadata, and improve HTML structure in Layout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000,
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  site: 'https://igrejacristaalvo.com.br',
+
+  integrations: [sitemap()]
 });
